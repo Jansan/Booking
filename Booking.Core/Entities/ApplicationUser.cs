d0d4b5401ecd19221048a08e19da8aa33b60498f
Jansan <jansan-26@hotmail.com>
2020-11-04 09:30:40 +0100

    Fix Nugets, Register ApplicationUser in Start

--- Booking.Core/Entities/ApplicationUser.cs
+++ Booking.Core/Entities/ApplicationUser.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Booking.Core.Entities
 {
